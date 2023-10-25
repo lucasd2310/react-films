0b1e94a1dc1994bde852701c82383b8dcf79b02f
Lucas Diaz <lucas.diaz0@walmart.com>
2023-10-25 15:23:54 -0300

    adding movie details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,24 +1,43 @@
-import { useEffect, useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import { get } from '../data/httpClient'
+import { getMovieImg } from '../utils/getMovieImg'
 
 export function MovieDetails() {
   const { movieId } = useParams()
-  const [movie, setMovie] = useState(null)
+  const [movie, setMovie] = useState([])
+  const [genre, setGenre] = useState([])
 
   useEffect(() => {
     get(`movie/${movieId}`).then((data) => {
       setMovie(data)
-      console.log('movie:', movie)
+      setGenre(data.genres[0])
+      console.log('data:', data)
     })
-    
   }, [movieId])
+
+  const imageUrl = getMovieImg(movie.poster_path, 500)
 
   return (
     <div>
-
-      <h1>{movie.title}</h1>
-      
+      <img
+        src={imageUrl}
+        alt={movie.title}
+      />
+      <div>
+        <p>
+          <strong>Title: </strong>
+          {movie.title}
+        </p>
+        <p>
+          <strong>Genre: </strong>
+          {genre.name}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          {movie.overview}
+        </p>
+      </div>
     </div>
   )
 }
